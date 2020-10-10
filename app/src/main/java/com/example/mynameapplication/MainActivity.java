5415ff14a9ef6de865cf20d6db680f207d361309
Justin Wilcox <justinwilcox95@hotmail.com>
2020-10-09 21:39:07 -0700

    main activity part 1

--- app/src/main/java/com/example/mynameapplication/MainActivity.java
+++ app/src/main/java/com/example/mynameapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mynameapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import android.widget.TextView;
+import android.widget.EditText;
+//import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sayHello(View view){
-        Log.i( "INFO", "Hello World!" );
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("Hello World!");
+    public void LogIn(View view){
+
+        Log.i( "INFO", "Logging in to " + Activity2.class.getName());
+        EditText user = findViewById(R.id.Username);
+        EditText pass = findViewById(R.id.Password);
+        String theusername = user.getText().toString();
+        String thepassword = pass.getText().toString();
+        Log.i("INFO", "Username is " + user.getText().toString());
+        Log.i("INFO", "Password is " + pass.getText().toString());
+        Intent intent = new Intent(getApplicationContext(),Activity2.class);
+        intent.putExtra("USERNAME", theusername);
+        intent.putExtra("PASSWORD", thepassword);
+        startActivity(intent);
+
+        // This was first lesson TextView tv = findViewById(R.id.textView);
+       // This was first lesson tv.setText("Hello World!");
     }
 }
