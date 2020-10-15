2728d5d3dc1bfdffeecfbe249e7463e4c03b5a22
Justin Wilcox <justinwilcox95@hotmail.com>
2020-10-14 17:41:38 -0700

    Made my list view

--- app/src/main/java/com/example/mynameapplication/MainActivity.java
+++ app/src/main/java/com/example/mynameapplication/MainActivity.java
@@ -7,20 +7,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 //import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood", "Renton", "Redmond", "Spokane", "Vancouver", "Tacoma", "Olympia",
+            "Bellingham", "Arlington", "Everett", "Woodinville", "Monroe", "New Castle", "Ballard", "Lacey"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.city, cities);
+        ListView lv = findViewById(R.id.city_list);
+        lv.setAdapter(adapter);
     }
 
     public void LogIn(View view){
 
-        Log.i( "INFO", "Logging in to " + Activity2.class.getName());
+
+        /*Log.i( "INFO", "Logging in to " + Activity2.class.getName());
         EditText user = findViewById(R.id.Username);
         EditText pass = findViewById(R.id.Password);
         String theusername = user.getText().toString();
@@ -30,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),Activity2.class);
         intent.putExtra("USERNAME", theusername);
         intent.putExtra("PASSWORD", thepassword);
-        startActivity(intent);
+        startActivity(intent);*/
+
+
 
         // This was first lesson TextView tv = findViewById(R.id.textView);
        // This was first lesson tv.setText("Hello World!");
